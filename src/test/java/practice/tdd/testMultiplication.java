85c4a8298688144234880d35011d088a2165373c
KimHyoJeong <khj444453@gmail.com>
2022-03-20 11:44:56 +0900

    중복제거 후 첫 번째 테스트 완료

--- src/test/java/practice/tdd/testMultiplication.java
+++ src/test/java/practice/tdd/testMultiplication.java
@@ -10,7 +10,7 @@ public class testMultiplication {
 
     < 할일 목록 >
     $5 + 10CHF = $10(환율이 2:1일 경우)
-    $5 * 2 = $10
+    -- $5 * 2 = $10
     amount를 private으로 만들기
     Dollar 부작용(side effect)?
     Money 반올림?
