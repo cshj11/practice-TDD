3c21862d14f24259b43a8da1f286533d76e8911c
KimHyoJeong <khj444453@gmail.com>
2022-03-23 21:40:54 +0900

    + 10CHF = (환율이 2:1일 경우)

--- src/main/java/practice/tdd/Money.java
+++ src/main/java/practice/tdd/Money.java
@@ -14,11 +14,11 @@ public class Money implements Expression{
         return amount == money.amount && currency().equals(money.currency());
     }
 
-    Money times(int multiplier){
+    Expression times(int multiplier){
         return new Money(amount * multiplier, currency);
     }
 
-    Expression plus(Money addend){
+    public Expression plus(Expression addend){
         return new Sum(this, addend);
     }
 
--- src/test/java/practice/tdd/testMultiplication.java
+++ src/test/java/practice/tdd/testMultiplication.java
@@ -9,7 +9,7 @@ public class testMultiplication {
     다중 통화를 지원하는 보고서 만들기
 
     < 할일 목록 >
-    $5 + 10CHF = $10(환율이 2:1일 경우)
+    -- $5 + 10CHF = $10(환율이 2:1일 경우)
     -- $5 * 2 = $10
     -- amount를 private으로 만들기
     -- Dollar 부작용(side effect)?
@@ -28,7 +28,9 @@ public class testMultiplication {
     $5 + $5에서 Money 반환하가
     -- Bank.reduce(Money)
     -- Money에 대한 통화 변환을 수행하는 Reduce
-    Reduce(Bank, String)
+    -- Reduce(Bank, String)
+    Sum.plus
+    Expression.times
     */
 
     @Test
@@ -100,6 +102,15 @@ public class testMultiplication {
     @Test
     public void testIdentityRate(){
         assertEquals(1, new Bank().rate("USD","USD"));
+    }
+
+    @Test void testMixedAddition(){
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
     }
 }
 
